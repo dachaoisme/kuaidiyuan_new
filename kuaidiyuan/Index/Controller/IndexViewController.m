afdc74a2fe78d07cf2807694b0e9e634ea617a41
dachaoisme <1522773761@qq.com>
2016-06-11 23:17:49 +0800

    完成所有接口，放到了首页里面

--- kuaidiyuan/Index/Controller/IndexViewController.m
+++ kuaidiyuan/Index/Controller/IndexViewController.m
@@ -7,8 +7,9 @@
 //
 
 #import "IndexViewController.h"
-
 #import "LoginViewController.h"
+#import "AccountUserModel.h"
+#import "IndexModel.h"
 @interface IndexViewController ()
 {
     
@@ -36,40 +37,215 @@
     [self creatRightNavWithImageName:@"nav_icon_msg"];
    
 }
-
-
-#pragma mark - 我的
--(void)leftItemActionWithBtn:(UIButton *)sender
+#pragma mark -接口相关
+#pragma mark - 申请为快递员
+-(void)applyToCourier
 {
-    BOOL yesLogin = [UserAccountManager sharedInstance].isLogin;
-    //需要先判断是否已经登陆
-    if (yesLogin) {
-        //收件箱界面
-        //弹出我的界面
-       
-    }else{
-        //进入登陆注册页面
-        
-        
-    }
-    
-    
+    /*
+     
+     realname  string  必需    真实姓名
+     IDcard    string  必需    身份证号码
+     college   string  必需    学校
+     grade     string  必需    年级
+     major     string  必需    专业
+     studentID string  必需    学号
+     */
+    ApplyCourierModel * courierModel = [[ApplyCourierModel alloc]initWithDic:nil];
+    NSMutableDictionary * dic = [NSMutableDictionary dictionary];
+    [dic setValue:[UserAccountManager sharedInstance].userId forKey:@"user_id"];
+    [dic setValue:courierModel.courierRealName forKey:@"realname"];
+    [dic setValue:courierModel.courierIdentityCard forKey:@"IDcard"];
+    [dic setValue:courierModel.courierCollege forKey:@"college"];
+    [dic setValue:courierModel.courierClass forKey:@"grade"];
+    [dic setValue:courierModel.courierMajor forKey:@"major"];
+    [dic setValue:courierModel.courierStudentID forKey:@"studentID"];
+    [[HttpClient sharedInstance]applyToCourierWithParams:dic withSuccessBlock:^(HttpResponseCodeModel *model) {
+        if (model.responseCode==ResponseCodeSuccess) {
+            ///申请成功
+        }else{
+            ///申请失败
+            [CommonUtils showToastWithStr:model.responseMsg];
+        }
+    } withFaileBlock:^(NSError *error) {
+        [CommonUtils showToastWithStr:@"请检查您的网络"];
+    }];
 }
-#pragma mark - 收件箱
--(void)rightItemActionWithBtn:(UIButton *)sender
+
+#pragma mark - 登陆
+-(void)login
 {
+    /*
+     telphnoe   string  必需    手机号码
+     passwd     string  必需    密码
+     */
+    NSMutableDictionary * dic = [NSMutableDictionary dictionary];
+    [dic setValue:@"手机号" forKey:@"telphnoe"];
+    [dic setValue:@"密码" forKey:@"passwd"];
     
-    BOOL yesLogin = [UserAccountManager sharedInstance].isLogin;
-    //需要先判断是否已经登陆
-    if (yesLogin) {
-        //收件箱界面
-        [CommonUtils showToastWithStr:@"收件箱"];
-    }else{
-        //进入登陆注册页面
+    [[HttpClient sharedInstance]loginWithParams:dic withSuccessBlock:^(HttpResponseCodeModel *model) {
+        if (model.responseCode==ResponseCodeSuccess) {
+            ///登陆成功
+        }else{
+            ///登陆失败
+            [CommonUtils showToastWithStr:model.responseMsg];
+        }
+    } withFaileBlock:^(NSError *error) {
+        [CommonUtils showToastWithStr:@"请检查您的网络"];
+    }];
+}
+#pragma mark - 送快递记录
+-(void)expressHistory
+{
+    NSMutableArray *expressHistoryArr = [NSMutableArray array];
+    NSMutableDictionary * dic = [NSMutableDictionary dictionary];
+    [[HttpClient sharedInstance]expressHistoryWithParams:dic withSuccessBlock:^(HttpResponseCodeModel *responseModel, HttpResponsePageModel *pageModel, NSDictionary *ListDic) {
+        if (responseModel.responseCode==ResponseCodeSuccess) {
+            NSArray * dicArr = [ListDic objectForKey:@"lists"];
+            for (NSDictionary * dic in dicArr) {
+                ExpressInfoModel *expressInfoModel = [[ExpressInfoModel alloc] initWithDic:dic];
+                [expressHistoryArr addObject:expressInfoModel];
+            }
+        }else{
+            [CommonUtils showToastWithStr:responseModel.responseMsg];
+        }
+    } withFaileBlock:^(NSError *error) {
         
-        LoginViewController * loginVC = [[LoginViewController alloc]init];
-        [self.navigationController pushViewController:loginVC animated:YES];
-    }
+    }];
+}
+
+#pragma mark - 设置快递员上下班
+-(void)configureCouriserWorkTime
+{
+    /*
+     courier_id      int         必需      快递员序号
+     working         int         必需      设置上下班 1上班 0下班
+     */
+    NSMutableDictionary * dic = [NSMutableDictionary dictionary];
+    [dic setValue:[UserAccountManager sharedInstance].userId forKey:@"courier_id"];
+    [dic setValue:@"1" forKey:@"working"];
+    
+    [[HttpClient sharedInstance]configureWorkTimeWithParams:dic withSuccessBlock:^(HttpResponseCodeModel *model) {
+        if (model.responseCode==ResponseCodeSuccess) {
+            ///设置成功
+            [CommonUtils showToastWithStr:@"设置成功"];
+        }else{
+            ///设置失败失败
+            [CommonUtils showToastWithStr:model.responseMsg];
+        }
+    } withFaileBlock:^(NSError *error) {
+        [CommonUtils showToastWithStr:@"请检查您的网络"];
+    }];
+}
+
+#pragma mark - 发快件取件消息接口
+-(void)expressMessageList
+{
+    NSMutableArray *expressHistoryArr = [NSMutableArray array];
+    NSMutableDictionary * dic = [NSMutableDictionary dictionary];
+    [[HttpClient sharedInstance]expressMessageWithParams:dic withSuccessBlock:^(HttpResponseCodeModel *responseModel, HttpResponsePageModel *pageModel, NSDictionary *ListDic) {
+        if (responseModel.responseCode==ResponseCodeSuccess) {
+            NSArray * dicArr = [ListDic objectForKey:@"lists"];
+            for (NSDictionary * dic in dicArr) {
+                ExpressMessageModel *expressInfoModel = [[ExpressMessageModel alloc] initWithDic:dic];
+                [expressHistoryArr addObject:expressInfoModel];
+            }
+        }else{
+            [CommonUtils showToastWithStr:responseModel.responseMsg];
+        }
+    } withFaileBlock:^(NSError *error) {
+        
+    }];
+}
+
+
+#pragma mark - 短信模板列表
+-(void)messageTemplatesList
+{
+    NSMutableArray *messageTemplateModelArr = [NSMutableArray array];
+    NSMutableDictionary * dic = [NSMutableDictionary dictionary];
+    [[HttpClient sharedInstance]messageTemplatesListWithParams:dic withSuccessBlock:^(HttpResponseCodeModel *responseModel, HttpResponsePageModel *pageModel, NSDictionary *ListDic) {
+        if (responseModel.responseCode==ResponseCodeSuccess) {
+            NSArray * dicArr = [ListDic objectForKey:@"lists"];
+            for (NSDictionary * dic in dicArr) {
+                MessageTemplateModel *expressInfoModel = [[MessageTemplateModel alloc] initWithDic:dic];
+                [messageTemplateModelArr addObject:expressInfoModel];
+            }
+        }else{
+            [CommonUtils showToastWithStr:responseModel.responseMsg];
+        }
+    } withFaileBlock:^(NSError *error) {
+        
+    }];
+}
+
+#pragma mark - 添加短信模板
+-(void)addMessageTemplate
+{
+    /*
+     user_id     int      必需    用户序号
+     template    string   必需    模板内容
+     */
+    NSMutableDictionary * dic = [NSMutableDictionary dictionary];
+    [dic setValue:[UserAccountManager sharedInstance].userId forKey:@"user_id"];
+    [dic setValue:@"欢迎光临，欢迎光临欢迎光临欢迎光临" forKey:@"template"];
+    
+    [[HttpClient sharedInstance]addMessageTemplatesWithParams:dic withSuccessBlock:^(HttpResponseCodeModel *model) {
+        if (model.responseCode==ResponseCodeSuccess) {
+            ///设置成功
+            [CommonUtils showToastWithStr:@"添加模板成功"];
+        }else{
+            ///设置失败失败
+            [CommonUtils showToastWithStr:model.responseMsg];
+        }
+    } withFaileBlock:^(NSError *error) {
+        [CommonUtils showToastWithStr:@"请检查您的网络"];
+    }];
+}
+
+#pragma mark - 修改短信模板
+-(void)updateMessageTemplate
+{
+    /*
+     id          int      必需    模板序号
+     template    string   必需    模板内容
+     */
+    NSMutableDictionary * dic = [NSMutableDictionary dictionary];
+    [dic setValue:@"模板序号" forKey:@"id"];
+    [dic setValue:@"欢迎光临，欢迎光临欢迎光临欢迎光临" forKey:@"template"];
+    
+    [[HttpClient sharedInstance]updateMessageTemplatesWithParams:dic withSuccessBlock:^(HttpResponseCodeModel *model) {
+        if (model.responseCode==ResponseCodeSuccess) {
+            ///设置成功
+            [CommonUtils showToastWithStr:@"更新成功"];
+        }else{
+            ///设置失败失败
+            [CommonUtils showToastWithStr:model.responseMsg];
+        }
+    } withFaileBlock:^(NSError *error) {
+        [CommonUtils showToastWithStr:@"请检查您的网络"];
+    }];
+}
+
+#pragma mark - 删除短信模板
+-(void)deleteMessageTemplate
+{
+    /*
+     id          int      必需    模板序号
+     */
+    NSMutableDictionary * dic = [NSMutableDictionary dictionary];
+    [dic setValue:@"模板序号" forKey:@"id"];
+    
+    [[HttpClient sharedInstance]deleteMessageTemplatesWithParams:dic withSuccessBlock:^(HttpResponseCodeModel *model) {
+        if (model.responseCode==ResponseCodeSuccess) {
+            ///设置成功
+            [CommonUtils showToastWithStr:@"删除成功"];
+        }else{
+            ///设置失败失败
+            [CommonUtils showToastWithStr:model.responseMsg];
+        }
+    } withFaileBlock:^(NSError *error) {
+        [CommonUtils showToastWithStr:@"请检查您的网络"];
+    }];
 }
 
 - (void)didReceiveMemoryWarning {
--- kuaidiyuan/Index/Model/IndexModel.h
+++ kuaidiyuan/Index/Model/IndexModel.h
@@ -11,3 +11,54 @@
 @interface IndexModel : NSObject
 -(id)initWithDic:(NSDictionary *)dic;
 @end
+
+///快递信息model
+@interface ExpressInfoModel : NSObject
+/*
+ "id": 1,
+ "express_type": 1,      //快递类型 具体数字含义 需求未定
+ "express_no": "323232", //快递单号
+ "status": 1,            //状态:0 未送达 1 已送达
+ "create_at": "2016-06-09 17:16:15"
+ 
+ */
+@property(nonatomic,strong)NSString *expressId;
+@property(nonatomic,strong)NSString *expressType;
+@property(nonatomic,strong)NSString *expressNum;
+@property(nonatomic,strong)NSString *expressStrtus;
+@property(nonatomic,strong)NSString *expressCreateTime;
+-(id)initWithDic:(NSDictionary *)dic;
+@end
+
+///取件消息model
+@interface ExpressMessageModel : NSObject
+/*
+ "id": 1,
+ "title": "dafds1111",
+ "msg": "dsafadsfa",
+ "create_at": "2016-06-10 09:46:38"
+ 
+ */
+@property(nonatomic,strong)NSString *expressMessageId;
+@property(nonatomic,strong)NSString *expressMessageTitle;
+@property(nonatomic,strong)NSString *expressMessageMsg;
+@property(nonatomic,strong)NSString *expressMessageCreateTime;
+-(id)initWithDic:(NSDictionary *)dic;
+@end
+
+///短信模板model 
+@interface MessageTemplateModel : NSObject
+/*
+ "id": 2,
+ "user_id": 1,
+ "template": "dadag",
+ "create_at": "2016-06-08 06:25:15"
+ 
+ */
+@property(nonatomic,strong)NSString *messageTemplateId;
+@property(nonatomic,strong)NSString *messageTemplateUserId;
+@property(nonatomic,strong)NSString *messageTemplateTemplate;
+@property(nonatomic,strong)NSString *messageTemplateCreateTime;
+-(id)initWithDic:(NSDictionary *)dic;
+@end
+
